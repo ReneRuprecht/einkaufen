524ca150584c3718bf0b983549def6d323cf6730
Rene Ruprecht <rene.ruprecht.396@gmail.com>
2024-09-29 15:38:51 +0200

    init spring project

---
+++ src/main/java/com/example/einkaufen/EinkaufenApplication.java
@@ -0,0 +1,13 @@
+package com.example.einkaufen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinkaufenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/einkaufen/EinkaufenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.einkaufen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinkaufenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
